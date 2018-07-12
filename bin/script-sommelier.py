b8968340510daacfed4e00d540169aaab0422e98
Eddy Snow <eddysnow@googlemail.com>
2018-07-12 14:32:19 +0100

    logic to match recommendation with wine list

--- bin/script-sommelier.py
+++ bin/script-sommelier.py
@@ -2,29 +2,49 @@
 
 import yaml
 import random
+#from collections import Counter
 
-with open("./conf/wines.yml", 'r') as wine_data:
+with open("./conf/recommendations.yml", 'r') as recommend_data:
     try:
-        wines = yaml.load(wine_data)
-        wine_len = (len(wines) -1)
-        rand_wine = random.randint(0, wine_len)
+        recommends = yaml.load(recommend_data)
+        recommends_len = (len(recommends) -1)
+        rand_recommend = random.randint(0, recommends_len)
+        recommend_desc = recommends[rand_recommend]["description"]
+        recommend_tags = recommends[rand_recommend]["tags"]
 
-        wine_name = wines[rand_wine]["name"]
-        wine_orig = wines[rand_wine]["origin"]
-        wine_label = wines[rand_wine]["label"]
-        wine_cost = wines[rand_wine]["cost"]
-        wine_grape = wines[rand_wine]["grape"]
-        wine_year = wines[rand_wine]["year"]
-        wine_desc = wines[rand_wine]["desc"]
-        wine_times_used = wines[rand_wine]["meta"]["times_used"]
+        print(recommend_desc)
 
-        print wine_name
-        print wine_orig
-        print wine_label
-        print wine_cost
-        print wine_grape
-        print wine_year
-        print wine_desc
+        with open("./conf/wines.yml", 'r') as wine_data:
+            try:
+                wines_to_return = set([])
+                wines_to_suggest = []
+                wines = yaml.load(wine_data)
 
-    except yaml.YAMLError as exception:
-        print(exception)
+                for wine in wines:
+                    for tag in wine["tags"]:
+                        if tag in recommend_tags:
+                            wines_to_return.add(wine["index"])
+
+                for wine_suggestion in list(wines_to_return):
+
+                    wine_dict = {
+                        'Name': wines[wine_suggestion]["name"],
+                        'Origin': wines[wine_suggestion]["origin"],
+                        'Label': wines[wine_suggestion]["label"],
+                        'Cost': wines[wine_suggestion]["cost"],
+                        'Grape': wines[wine_suggestion]["grape"],
+                        'Year': wines[wine_suggestion]["year"],
+                        'Description': wines[wine_suggestion]["desc"],
+                        'ImageUrl': wines[wine_suggestion]["url"]
+                    }
+
+                    wines_to_suggest.append(wine_dict)
+
+                for x in wines_to_suggest:
+                    print(x['ImageUrl'])
+
+            except yaml.YAMLError as wine_exception:
+                print(wine_exception)
+
+    except yaml.YAMLError as rec_exception:
+        print(rec_exception)
